d12152010abed246f149405bb94d0ca50a7f05cc
BugHunter-X <askzenai@gmail.com>
2025-02-19 13:02:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,8 @@
+```javascript
+// Example showing incorrect Tailwind class usage
+<div class="bg-red-500 p-4 text-white">
+  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    Click Me
+  </button>
+</div>
+```
